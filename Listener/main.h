9d728fd3d66c6c8b3a1f1a0239028f2f4cfc5b21
Jeremy Rand <jeremy@rand-family.com>
2021-07-16 12:37:38 -0400

    Initial Commit

---
+++ Listener/main.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ * Listener
+ *
+ * Created by Jeremy Rand on 2021-07-16.
+ * Copyright (c) 2021 ___ORGANIZATIONNAME___. All rights reserved.
+ *
+ */
+
+#ifndef _GUARD_PROJECTListener_FILEmain_
+#define _GUARD_PROJECTListener_FILEmain_
+
+
+#define windowRes 1001
+
+
+#endif /* defined(_GUARD_PROJECTListener_FILEmain_) */
