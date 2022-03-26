f36bb10f5435089edbf0447af04355284bc06c84
Jeremy Rand <jeremy@rand-family.com>
2022-03-26 12:37:29 -0400

    Add support for Versions.

--- Listener/main.h
+++ Listener/main.h
@@ -13,5 +13,16 @@
 
 #define windowRes 1001
 
+/* Update Info */
+#define UPDATE_INFO 1
+
+
+/* Strings */
+#define DISPLAY_NAME_STR        2000
+#define AUTHOR_NAME_STR         2001
+#define DOWNLOAD_URL_STR        2002
+#define ARCHIVE_STR             2003
+#define VERSIONS_DATA_FILE_STR  2004
+
 
 #endif /* defined(_GUARD_PROJECTListener_FILEmain_) */
